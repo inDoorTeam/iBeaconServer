e355e75886c7ed20bd3d9d8482962c4e73c4d11f
bingxyz <bingxyz1024@gmail.com>
2016-06-07 16:51:05 +0800

    新增傳送好友位置

--- src/iBeaconServer/ClientHandler.java
+++ src/iBeaconServer/ClientHandler.java
@@ -68,7 +68,6 @@ public class ClientHandler implements Runnable {
                                 friendLocationJSONArray.put(JSONObject);
                             }
                         }
-                        int i = 1;
                         friendLocationJSONObject.put(JSON.KEY_USER_LIST, friendLocationJSONArray);
                         System.out.println(friendLocationJSONObject);
                         client.send(friendLocationJSONObject.toString());
