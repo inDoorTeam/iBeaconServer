335dccce54796569d1cc755da5d056c1a6c082f3
x2413915 <x2413915@gmail.com>
2016-07-19 23:08:59 +0800

    區分好友與非好友

--- src/iBeaconServer/Server.java
+++ src/iBeaconServer/Server.java
@@ -9,8 +9,8 @@ public class Server{
     public static void main(String[] args) throws IOException {
         // TODO Auto-generated method stub
         int SOCKET_PORT = 8766;
+        String s = "\uD83D\uDC65";
         new Thread(new NetworkService(SOCKET_PORT, 100)).run();
-//        GuideDB.getInstance().selectFromItemList();
     }
     
 }
